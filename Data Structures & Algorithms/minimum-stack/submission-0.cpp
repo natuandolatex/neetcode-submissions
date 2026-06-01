8e75d2abefb4d6ac0155cb8c9f994976a8c25698
Nguyễn Anh Tuấn <natuan@dhsphue.edu.vn>
2026-06-02 00:34:58 +0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,30 @@
+class MinStack {
+private:
+    stack<int> st;
+    stack<int> minst;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if (minst.empty()){
+            minst.push(val);
+        }
+        else minst.push(min(minst.top(), val));
+    }
+    
+    void pop() {
+        st.pop();
+        minst.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minst.top();
+    }
+};
