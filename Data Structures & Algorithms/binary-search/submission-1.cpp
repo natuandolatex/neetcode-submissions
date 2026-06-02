e24804fed18509078bba5c8cd336519ad452f697
Nguyễn Anh Tuấn <natuan@dhsphue.edu.vn>
2026-06-02 22:15:15 +0700

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0; int r = nums.size();
+        while (l < r){
+            int mid = (l+r)/2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > target){
+                r = mid;
+            }
+            else l = mid+1;
+        }
+        return -1;
+    }
+};
