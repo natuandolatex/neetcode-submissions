a12c7d7856a730979e2fbe96f30c8f610feb6631
Nguyễn Anh Tuấn <natuan@dhsphue.edu.vn>
2026-06-07 21:28:54 +0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = prices[0], profit = 0;
+        for (int i = 0; i < prices.size(); i++){
+            profit = max(profit, prices[i] - minPrice);
+            minPrice = min(minPrice, prices[i]);
+        }
+        return profit;
+    }
+};
